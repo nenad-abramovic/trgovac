34c4bb40f8a5257138b7f4564035da33c2b5d083
Nenad Abramovic <nenad@null.net>
2020-05-23 10:13:59 +0200

    Minor fix

--- api/server.js
+++ api/server.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const pool = require('./db');
-const cors = require('cors');
 const usersRouter = require('./routes/users/users');
 const adsRouter = require('./routes/ads/ads');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
 
 app.use(express.static('build'));
 
 app.use('/users', usersRouter);
 app.use('/ads', adsRouter);
-
-app.get('/data', async (req, res, next) => {
-  try {
-    let data = await pool.query({
-      text: 'SELECT name FROM categories',
-      rowMode: 'array'
-    });
-    return res.json(data.rows.flat());
-  } catch (e) {
-    return res.json(e);
-  }
-});
 
 app.listen(process.env.PORT, () => {
   console.log('server started');
