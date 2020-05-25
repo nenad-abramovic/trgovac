2e48e30a3af341e67c10ce24e5dead8b19859106
Nenad Abramovic <nenad@null.net>
2020-05-25 23:05:14 +0200

    Minor changes

--- src/components/search/SearchElement.jsx
+++ src/components/search/SearchElement.jsx
@@ -6,11 +6,13 @@ const SearchElement = ({ ad }) => {
   return (
     <div className={styles.container}>
       <div>
-        <Link to={`/ad/${ad.ad_uuid}`}><h4 className={styles.title}>{ad.title}</h4></Link>
-  <p>цена: {ad.price}</p>
-        <p>{ad.place} - {ad.created_at} - {ad.fullname} - {ad.phone_number}</p>
+        <Link to={`/ad/${ad.ad_uuid}`}><h3 className={styles.title}>{ad.title}</h3></Link>
+        <p>цена: {ad.price}</p>
+        <p>датум објављивања: {new Date(ad.created_at).toLocaleString("sr-Cyrl")}</p>
+        <p>продавац: {ad.fullname}</p>
+        <p>број телефона: {ad.phone_number}</p>
       </div>
-      <img style={{ width: '200px', height: '200px' }} src={`data:image/png;base64,${btoa(String.fromCharCode(...new Uint8Array(ad.image.data)))}`} alt={`Слика за ${ad.title}`} />
+      <img src={`data:image/png;base64,${btoa(String.fromCharCode(...new Uint8Array(ad.image.data)))}`} alt={`Слика за ${ad.title}`} />
     </div>
   );
 }
