0e3cd0bee4b69b38bb3229a2bfc48079c18c4b30
Nenad Abramovic <nenad@null.net>
2020-05-26 16:03:39 +0200

    Build app

---
+++ build/precache-manifest.4945afb979822d9727998426da3b500a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e6a6487f8fd04f62f1dac53fa66e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1721587883ba96953b",
+    "url": "/static/css/main.beb279a3.chunk.css"
+  },
+  {
+    "revision": "bc6bb222cff5bc20d389",
+    "url": "/static/js/2.7806008c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7806008c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1721587883ba96953b",
+    "url": "/static/js/main.3d690323.chunk.js"
+  },
+  {
+    "revision": "1ccbc16911324042c81a",
+    "url": "/static/js/runtime-main.30dfe866.js"
+  },
+  {
+    "revision": "418d6668afcddb7714104f43bfb39657",
+    "url": "/static/media/AnonymousPro-Regular.418d6668.ttf"
+  }
+]);
