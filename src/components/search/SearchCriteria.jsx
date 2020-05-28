abddd973241b13ecaffb413ae93396a5a449e7a3
Nenad Abramovic <nenad@null.net>
2020-05-28 21:33:33 +0200

    Minor fix

--- src/components/search/SearchCriteria.jsx
+++ src/components/search/SearchCriteria.jsx
@@ -24,24 +24,32 @@ const SearchCriteria = ({ filterAds }) => {
   return (
     <div className={styles.container}>
       <div>
-        <select id="category" onChange={selectCriteria}>
-          <option value="%">изабери категорију</option>
-          {categories.data.map((category) => (
-            <option key={category.category_uuid} value={category.name}>
-              {category.name.toLowerCase()}
-            </option>
-          ))}
-        </select>
+        {categories.success ? (
+          <select id="category" onChange={selectCriteria}>
+            <option value="%">изабери категорију</option>
+            {categories.data.map((category) => (
+              <option key={category.category_uuid} value={category.name}>
+                {category.name.toLowerCase()}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <p>{categories.message}</p>
+        )}
       </div>
       <div>
-        <select id="place" onChange={selectCriteria}>
-          <option value="%">изабери место</option>
-          {places.data.map((place) => (
-            <option key={place.place_uuid} value={place.name}>
-              {place.name.toLowerCase()}
-            </option>
-          ))}
-        </select>
+        {places.success ? (
+          <select id="place" onChange={selectCriteria}>
+            <option value="%">изабери место</option>
+            {places.data.map((place) => (
+              <option key={place.place_uuid} value={place.name}>
+                {place.name.toLowerCase()}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <p>{places.message}</p>
+        )}
       </div>
     </div>
   );
