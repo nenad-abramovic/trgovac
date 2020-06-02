4e994da3682f23b23812fa6f8874f115030035cc
[object Object] <nenad@null.net>
2020-06-02 08:47:11 +0200

    Minor changes

--- api/routes/ads/commentAd.js
+++ api/routes/ads/commentAd.js
@@ -19,16 +19,19 @@ const commentAd = async (req, res, next) => {
       });
     }
 
+    console.log("a");
     let userData = await pool.query({
       text: "SELECT user_uuid FROM users WHERE email=$1",
       values: [email],
     });
 
+    console.log("b", userData);
     await pool.query({
       text: "INSERT INTO comments(text, user_uuid, ad_uuid) VALUES($1, $2, $3)",
       values: [req.body.text, userData.rows[0].user_uuid, req.body.ad_uuid],
     });
 
+    console.log("c");
     return res.status(201).json({
       success: true,
     });
