b003ef2ddb0eac504d3120b5c946aba5dad01abf
[object Object] <nenad@null.net>
2020-06-02 11:30:17 +0200

    Minor changes

--- api/routes/ads/getComments.js
+++ api/routes/ads/getComments.js
@@ -16,7 +16,7 @@ const getComments = async (req, res, next) => {
     let data = await pool.query({
       text:
         "SELECT text, created_at, fullname, user_uuid FROM comments JOIN users USING(user_uuid) WHERE ad_uuid=$1",
-      values: [req.params.adUUID],
+      values: [req.params.ad_uuid],
     });
 
     return res.status(200).json({
