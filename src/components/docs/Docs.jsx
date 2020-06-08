a5f099145d0860ba286719a9c56b4d3bb7724b3b
[object Object] <nenad@null.net>
2020-06-09 00:30:56 +0200

    Minor fix

--- src/components/docs/Docs.jsx
+++ src/components/docs/Docs.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-import spec from "./spec.json";
 import styles from "./Docs.module.css";
 
 export default () => (
   <div className={styles.container}>
-    <SwaggerUI spec={spec} />
+    <SwaggerUI url="https://trgovac.herokuapp.com" />
   </div>
 );
